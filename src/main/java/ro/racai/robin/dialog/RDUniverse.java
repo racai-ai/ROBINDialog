eb0fadf6b883338f66e23aab41d17ce31b2eb406
Radu Ion <ion.radu@outlook.com>
2019-07-17 11:07:56 +0300

    Added RDUniverse class

--- src/main/java/ro/racai/robin/dialog/RDUniverse.java
+++ src/main/java/ro/racai/robin/dialog/RDUniverse.java
@@ -10,7 +10,8 @@ import java.util.Map;
  * @author Radu Ion ({@code radu@racai.ro})
  * <p>This class describes the universe of discourse for a given
  * micro-word. That is, it maps {@link ro.racai.robin.dialog.RDConcept}s
- * to actual, textual descriptions of existing objects. For instance,
+ * to actual, textual descriptions of existing objects, being an <b>inventory</b>
+ * of ``known'' values for all concepts. For instance,
  * in our PRECIS scenario, for the <i>sală</i> concept, we could have
  * values such as <i>209</i>, <i>laboratorul de SDA</i>, etc.</p>
  * <p>This class has to be sub-classed so that the correspondence
@@ -19,5 +20,41 @@ import java.util.Map;
 public abstract class RDUniverse {
 	protected Map<RDConcept, List<String>> conceptToLiteral;
 	
-	public abstract void mapConcept(RDConcept concept);
+	/**
+	 * <p>Add a textual description (word or noun phrase) to
+	 * this concept, such that the description is an instantiation
+	 * of the concept in this universe of discourse.</p>
+	 * <p><b>Do not forget to add the concept to {@link #conceptToLiteral}
+	 * protected map field!</b></p>
+	 * @param concept       the concept to be mapped
+	 *                      (to receive new descriptions.
+	 */
+	public abstract void describeConcept(RDConcept concept);
+	
+	/**
+	 * <p>Returns {@code true} if the textual description of
+	 * the concept has been mapped in this universe of discourse.
+	 * In other words, it will test if the variable binding is
+	 * supported by this universe of discourse.</p>
+	 * @param description       the textual description of the concept;
+	 * @param concept           the {@link RDConcept} instance.
+	 * @return                  {@code true} if description is in the list
+	 *                          of mappings for the concept.
+	 */
+	public boolean isInstanceOf(String description, RDConcept concept) {
+		if (!conceptToLiteral.containsKey(concept)) {
+			// Concept has not been mapped in this universe.
+			return false;
+		}
+		
+		for (String literal : conceptToLiteral.get(concept)) {
+			if (description.equalsIgnoreCase(literal)) {
+				return true;
+			}
+		}
+		
+		// TODO: do partial matching, fuzzy matching, see
+		// where this simple matching fails.
+		return false;
+	}
 }
