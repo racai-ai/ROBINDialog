3917dfa3c92f5145debba621d826074f51450bd0
Radu Ion <rion@vantage.com>
2019-07-17 10:08:00 +0300

    First commit in the ROBIN Dialog project

---
+++ src/main/java/ro/racai/robin/dialog/RDManager.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ro.racai.robin.dialog;
+
+/**
+ * @author Radu Ion ({@code radu@racai.ro})
+ * <p>This is the main entry point for the ROBIN Dialogue manager.</p>
+ */
+public class RDManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
